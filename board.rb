d6ae9d0369ce10b73cb88d62d193313ff90612a3
Cassie Montoya <clm21020@gmail.com>
2015-02-25 10:59:17 -0800

    Added in_check?

--- board.rb
+++ board.rb
@@ -14,6 +14,9 @@ class Board
   def place_pieces
     place_non_pawns("black")
     place_non_pawns("white")
+
+    # self[2, 4] = Rook.new("black", [2, 4], self)
+    # self[6, 4] = Rook.new("white", [6, 4], self)
     # place_pawns("black")
     # place_pawns("white")
   end
@@ -31,21 +34,36 @@ class Board
     (0...SIZE).each do |i|
       self[x, i] = Pawn.new(color, [x, i], self)
     end
-
   end
 
   def in_check?(color)
-    # returns whether a player is in check.
-    # You can implement this by (1) finding the position of the king on the board then
-    # (2) seeing if any of the opposing pieces can move to that position.
-    (0...SIZE)
+    king = find_king(color)
+    other_color = (color == "black" ? "white" : "black")
+    find_team(other_color).each do |piece|
+      piece.moves.each do |pos|
+        return true if pos == king.pos
+      end
+    end
 
+    false
+  end
+
+  def find_king(color)
+    @grid.flatten.compact.find do |piece|
+      piece.class == King && piece.color == color
+    end
   end
 
   def move(start_pos, end_pos)
     # This should update the 2d grid and also the moved piece's position.
     # You'll want to raise an exception if: (a) there is no piece at start or
     # (b) the piece cannot move to end_pos.
+  end
+
+  def find_team(color)
+    @grid.flatten.compact.select do |piece|
+      piece.color == color
+    end
   end
 
   def [](x, y)
@@ -67,5 +85,9 @@ class Board
   def color_of_piece_at(pos)
     piece = @grid[pos[0]][pos[1]]
     piece.nil? ? nil : piece.color
+  end
+
+  def inspect
+    "I am a board"
   end
 end
