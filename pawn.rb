811336c3cad37b0c1d311dbf263bd3ad489a00c7
Cassie Montoya <clm21020@gmail.com>
2015-02-25 17:40:05 -0800

    Toggle the pawn

--- pawn.rb
+++ pawn.rb
@@ -2,7 +2,6 @@ class Pawn < Piece
   def initialize(color, pos, board)
     super
     @picture = color == "black" ? "♟" : "♙"
-    @moved_yet = false
   end
 
   def moves
@@ -18,11 +17,16 @@ class Pawn < Piece
     double_step = [@pos[0] + row_diff * 2, @pos[1]]
 
     forward_moves << single_step if @board.on_board?(single_step)
-    forward_moves << double_step if !@moved_yet && @board.piece_at(double_step).nil?
+    forward_moves << double_step if still_at_home? && @board.piece_at(double_step).nil?
 
     forward_moves.select do |pos|
       @board.piece_at(single_step).nil?
     end
+  end
+
+  def still_at_home?
+    start_row = @color == "black" ? 1 : 6
+    start_row == @pos[0]
   end
 
   def diagonal_moves(row_diff)
